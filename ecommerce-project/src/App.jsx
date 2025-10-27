83d8d6abb06ac8955aa6f7cfb01bbbcfe365944b
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-10-28 01:15:54 +0530

    ecommerce - Split the checkoutHeader and split all checkout componenets into sperate folder

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
-import CheckoutPage from './pages/CheckoutPage'
+import CheckoutPage from './pages/checkout/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
 
 function App() {
--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -1,32 +1,12 @@
-import { Link } from 'react-router'
-
-import './checkout-header.css'
 import './CheckoutPage.css'
+import CheckoutHeader from './CheckoutHeader'
 
 function CheckoutPage() {
     return (
         <>
             <title>Checkout</title>
-            
-            <div className="checkout-header">
-                <div className="header-content">
-                    <div className="checkout-header-left-section">
-                        <Link to="/">
-                            <img className="logo" src="images/logo.png" />
-                            <img className="mobile-logo" src="images/mobile-logo.png" />
-                        </Link>
-                    </div>
 
-                    <div className="checkout-header-middle-section">
-                        Checkout (<Link className="return-to-home-link"
-                            to="/">3 items</Link>)
-                    </div>
-
-                    <div className="checkout-header-right-section">
-                        <img src="images/icons/checkout-lock-icon.png" />
-                    </div>
-                </div>
-            </div>
+            <CheckoutHeader />
 
             <div className="checkout-page">
                 <div className="page-title">Review your order</div>
@@ -220,7 +200,7 @@ function CheckoutPage() {
                     </div>
                 </div>
             </div>
-        </> 
+        </>
     )
 }
 

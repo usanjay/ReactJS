5852d5adc2d268cd1fa0d6a4005eda4c21c4af38
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-10-28 01:55:45 +0530

    ecommerce - update <a> links to <Link>

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header"
+import {Link} from 'react-router'
 import '../components/Header.css'
 import './OrdersPage.css'
 
@@ -56,11 +57,11 @@ function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
 
                             <div className="product-image-container">
@@ -84,11 +85,11 @@ function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -135,11 +136,11 @@ function OrdersPage() {
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>

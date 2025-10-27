3a579fa06571bcc2fbb73705d4ff7ea7ef62b295
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-10-28 02:53:32 +0530

    ecommerce - Create Not Found Page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/checkout/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
 import TrackingPage from './pages/TrackingPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="checkout" element={<CheckoutPage />} />
         <Route path="orders" element={<OrdersPage />} />
         <Route path="tracking" element={<TrackingPage />}/>
+        <Route path="*" element={<NotFoundPage />}/>
       </Routes>
     </>
   )

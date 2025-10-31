eaa86ab4cd42c9f278996919eb0d466ac6dc2e06
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-11-01 02:07:24 +0530

    ecommerce - Generate the prograss bar based on the progress

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -6,19 +6,19 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import './TrackingPage.css'
 
-function TrackingPage({cart}) {
-    const {orderId, productId} = useParams();
+function TrackingPage({ cart }) {
+    const { orderId, productId } = useParams();
     const [order, setOrder] = useState(null);
 
-    useEffect(()=> {
+    useEffect(() => {
         const fetchOrderData = async () => {
             const response = await axios.get(`/api/orders/${orderId}?expand=products`);
             setOrder(response.data);
         }
         fetchOrderData();
-    },[orderId]);
+    }, [orderId]);
 
-    if(!order) {
+    if (!order) {
         return null;
     }
 
@@ -26,12 +26,24 @@ function TrackingPage({cart}) {
         return productId === orderProduct.productId
     });
 
+
+    const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+    const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+    let deliveryPercentage = (timePassedMs / totalDeliveryTimeMs) * 100;
+    // console.log(deliveryPercentage, timePassedMs, totalDeliveryTimeMs)
+
+    if (deliveryPercentage > 100) deliveryPercentage = 100;
+
+    const isPreparing = deliveryPercentage < 33;
+    const isShipped = deliveryPercentage >= 33 && deliveryPercentage < 100;
+    const isDelivered = deliveryPercentage === 100;
+
     return (
         <>
             <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
             <title>Tracking</title>
 
-            <Header cart={cart}/>
+            <Header cart={cart} />
 
             <div className="tracking-page">
                 <div className="order-tracking">
@@ -40,7 +52,10 @@ function TrackingPage({cart}) {
                     </Link>
 
                     <div className="delivery-date">
-                        Arriving on {
+                        {
+                            deliveryPercentage >= 100 ? 'Delivered on ' : 'Arriving on '
+                        }
+                        {
                             dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')
                         }
                     </div>
@@ -56,19 +71,20 @@ function TrackingPage({cart}) {
                     <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
-                        <div className="progress-label">
+                        <div className={`progress-label ${isPreparing && 'current-status'}`}>
                             Preparing
                         </div>
-                        <div className="progress-label current-status">
+                        <div className={`progress-label ${isShipped && 'current-status'}`}>
                             Shipped
                         </div>
-                        <div className="progress-label">
+                        <div className={`progress-label ${isDelivered && 'current-status'}`}>
                             Delivered
                         </div>
                     </div>
 
                     <div className="progress-bar-container">
-                        <div className="progress-bar"></div>
+                        <div className="progress-bar"
+                            style={{ width: `${deliveryPercentage}%` }}></div>
                     </div>
                 </div>
             </div>

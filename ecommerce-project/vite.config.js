105921f149d48078e46cb6b96146691031512e08
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-11-05 14:47:12 +0530

    ecommerce - save build result in ecommerce-backend

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
       '/images': {
         target: 'http://localhost:3000'
       }
-    }
+    },
+  },
+  build: {
+    outDir: '../ecommerce-backend/dist'
   }
 })
